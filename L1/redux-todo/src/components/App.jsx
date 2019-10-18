186de831acc69a69213d0210076d807a01c4088b
Evan Li <evan.li.2014@gmail.com>
2019-10-17 21:28:07 -0400

    chore: remove comments

--- L1/redux-todo/src/components/App.jsx
+++ L1/redux-todo/src/components/App.jsx
@@ -11,26 +11,12 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-
 function mapStateToProps(state) {
   return { todos: state.todos.list, status: state.todos.status };
 }
 
 const App = ({ onAddTodo, todos, status }) => {
   const [todoInput, setTodoInput] = useState('');
-  // const [noteInput, setNoteInput] = useState('');
-
-  // Suboptimal solution without thunk
-  // Problems with multiple clicks and bad state organization
-  // Also a lot of logic within the UI should be handlerd in the action creator instead
-  // function handleSubmitTodo(e) {
-  //   e.preventDefault();
-  //   dispatch(requestSubmitTodo(todoInput));
-  //   setTimeout(() => {
-  //     dispatch(addTodo(todoInput));
-  //     setTodoInput('');
-  //   }, Math.random() * 1000);
-  // }
 
   function handleSubmitTodo(e) {
     e.preventDefault();
@@ -44,24 +30,12 @@ const App = ({ onAddTodo, todos, status }) => {
     setTodoInput(e.target.value);
   }
 
-  // function handleSubmitNote(e) {
-  //   e.preventDefault();
-  //   dispatch(addNote(noteInput));
-  //   setNoteInput('');
-  // }
-
-  // function handleNoteInputChange(e) {
-  //   setNoteInput(e.target.value);
-  // }
-
   return (
     <>
       <form action=''>
         <input type='text' aria-label='todo-input' name='todo' onChange={handleTodoInputChange} value={todoInput} />
         <button type='submit' onClick={handleSubmitTodo}>Add TODO</button>
       </form>
-      { /* Probably better to build a generic list component
-         here, call mapStateToProps in this component and pass in props normally */}
       <TodoList todos={todos} status={status} />
     </>
   );
